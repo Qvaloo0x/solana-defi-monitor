c7717a583c82186e8ba3f82e079e9c7b88f16012
Jorge Luis Abreu Benitez <qvaloo19@gmail.com>
2026-02-17 10:20:12 -0500

    Update dex_volume_dominance.sql

--- queries/dex_volume_dominance.sql
+++ queries/dex_volume_dominance.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    date_trunc('day', block_time) AS date,
+    project AS dex_name,
+    sum(amount_usd) AS daily_volume_usd
+FROM dex_solana.trades
+WHERE block_time >= current_date - interval '30' day
+    AND amount_usd > 0
+GROUP BY 1, 2
+ORDER BY 1 DESC, 3 DESC
